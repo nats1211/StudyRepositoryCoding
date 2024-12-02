40abe756994577a686c2be9dbcd6191ec2af4ac6
Tristan James Burgos <burgostristan96@gmail.com>
2024-12-02 22:21:12 +0800

    tan reply

--- GitTesting/Program.cs
+++ GitTesting/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Kerby mamimigay pancit canton");
+            Console.WriteLine("ayoko nga -tan");
         }
     }
 }
