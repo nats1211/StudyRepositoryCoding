753e973b2ad3886f905402b3005d0816000b086c
Tristan James Burgos <burgostristan96@gmail.com>
2024-12-02 22:17:11 +0800

    another update

--- GitTesting/Program.cs
+++ GitTesting/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! c# -nats updating");
+            Console.WriteLine("Hello, World! c# new update");
         }
     }
 }
